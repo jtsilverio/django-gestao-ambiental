4ddffd688a8db0893a67803ba5d8a23733a38ba6
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-11-23 10:06:17 -0300

    refac: removing tp_fornecedor

---
+++ core/menus.py
@@ -0,0 +1,71 @@
+MENU_SIDEBAR = [
+    {
+        "name": "Dashboard",
+        "url": "home",
+        "icon": "bi bi-bar-chart-line-fill ",
+    },
+    {
+        "name": "Resíduos",
+        "url": "admin:index",
+        "icon": "bi bi-trash3-fill",
+        "submenu": [
+            {
+                "name": "Entrada",
+                "url": "entrada",
+            },
+            {
+                "name": "Saída",
+                "url": "saida",
+            },
+            {
+                "name": "Fornecedor",
+                "url": "fornecedor",
+            },
+            {
+                "name": "Classe",
+                "url": "classe",
+            },
+            {
+                "name": "Destinação",
+                "url": "destinacao",
+            },
+        ],
+    },
+    {
+        "name": "Eletricidade",
+        "url": "eletricidade:index",
+        "icon": "bi bi-lightning-charge-fill",
+    },
+    {
+        "name": "Água",
+        "url": "agua:index",
+        "icon": "bi bi-droplet-fill",
+    },
+    {
+        "name": "Combustível",
+        "url": "combustivel:index",
+        "icon": "bi bi-fuel-pump-fill",
+    },
+    {
+        "name": "Cluster",
+        "url": "cluster:index",
+        "icon": "bi bi-globe-americas",
+    },
+    {"name": "div"},
+    {
+        "name": "Tipo de Combustível",
+        "url": "tipo_combustivel:index",
+        "icon": "bi bi-tags-fill",
+    },
+    {
+        "name": "Unidade de Consumo",
+        "url": "unidade_consumo:index",
+        "icon": "bi bi-house-fill",
+    },
+    {"name": "div"},
+    {
+        "name": "Admin",
+        "url": "admin:index",
+        "icon": "bi bi-gear-fill",
+    },
+]
