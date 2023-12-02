0a1b2c7c32c45657683f293cdb19bc90edb75676
Jefferson Silverio <jefferson.silverio@syngenta.com>
2023-12-02 12:36:28 -0300

    add cidade field in Fornecedor

--- apps/fornecedor/models.py
+++ apps/fornecedor/models.py
@@ -5,6 +5,7 @@ from apps.destinacao.models import Destinacao
 
 class Fornecedor(models.Model):
     id_fornecedor = models.AutoField(primary_key=True)
+    data = models.DateField(null=False, blank=False)
     nome = models.CharField(max_length=50, null=False, blank=False)
     cidade = models.CharField(max_length=25, null=False, blank=False)
     destinacao = models.ManyToManyField(
