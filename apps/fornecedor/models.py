6c0b39ab13e267feb5f4768e42fd62bfa3b2fb02
Jefferson <jfo.txt@gmail.com>
2023-12-11 12:16:27 -0300

    fix Fornecedor Cadastro

--- apps/fornecedor/models.py
+++ apps/fornecedor/models.py
@@ -5,9 +5,8 @@ from apps.destinacao.models import Destinacao
 
 class Fornecedor(models.Model):
     id_fornecedor = models.AutoField(primary_key=True)
-    data = models.DateField(null=False, blank=False)
     nome = models.CharField(max_length=50, null=False, blank=False)
-    cidade = models.CharField(max_length=25, null=False, blank=False)
+    # cidade = models.CharField(max_length=25, null=False, blank=False)
     destinacao = models.ManyToManyField(
         Destinacao,
         db_table="fornecedor_destinacao",
