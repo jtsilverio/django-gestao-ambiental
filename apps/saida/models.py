1a5309b7e6952f08de04443ed141b251adfbfc41
Jefferson <jfo.txt@gmail.com>
2023-12-18 19:52:05 -0300

    Fix: Modify Saida to accomodate new changes in Fornecedor

--- apps/saida/models.py
+++ apps/saida/models.py
@@ -1,23 +1,36 @@
 from django.db import models
 
 from apps.cluster.models import Cluster
-from apps.fornecedor.models import Destinacao
+from apps.destinacao.models import Destinacao
+from apps.fornecedor.models import Fornecedor
 from apps.tipo_residuos.models import TipoResiduos
 
 
 # Create your models here.
 class Saida(models.Model):
     id_saida = models.AutoField(primary_key=True)
+    data = models.DateField(null=False, blank=False)
+    id_cluster = models.ForeignKey(Cluster, models.DO_NOTHING, db_column="id_cluster")
     id_tp_residuos = models.ForeignKey(
         TipoResiduos, models.DO_NOTHING, db_column="id_tp_residuos"
     )
-    id_cluster = models.ForeignKey(Cluster, models.DO_NOTHING, db_column="id_cluster")
+    peso = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, null=False, blank=False
+    )
+    id_fornecedor_destinacao = models.ForeignKey(
+        Fornecedor,
+        models.DO_NOTHING,
+        db_column="id_fornecedor_destinacao",
+        related_name="fornecedor_destinacao",
+    )
     id_destinacao = models.ForeignKey(
         Destinacao, models.DO_NOTHING, db_column="id_destinacao"
     )
-    data = models.DateField(null=False, blank=False)
-    peso = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0, null=False, blank=False
+    id_fornecedor_transporte = models.ForeignKey(
+        Fornecedor,
+        models.DO_NOTHING,
+        db_column="id_fornecedor_transporte",
+        related_name="fornecedor_transporte",
     )
     receita = models.DecimalField(
         max_digits=10, decimal_places=2, default=0, null=False, blank=True
